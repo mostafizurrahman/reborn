acb432a78a19c46e9c7bc0ec24ead8c2e4394f2c
Alamin <alamin.edu98@gmail.com>
2023-01-27 11:04:38 +0600

    Create a new project for carbd

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(myApp());
+
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Welcome"),
+          ),
+          body: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                        hintText: "Enter your name",
+                        border: OutlineInputBorder()),
+                  ),
+                  TextField(
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                      hintText: "Enter your phone number",
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  TextField(
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                      hintText: "Enter your email",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text("Submit"),
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
